430f0daf4a2a313623f2b5f13c97f920c6b87c3e
Gayal Melappilly <gayalsunil@gmail.com>
2025-05-14 05:44:58 +0530

    fix: env

--- pages/api/seller/create-profile.ts
+++ pages/api/seller/create-profile.ts
@@ -1,6 +1,8 @@
 'use server'
 
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { configDotenv } from 'dotenv';
+configDotenv()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end('Method Not Allowed');
--- pages/api/users/login.ts
+++ pages/api/users/login.ts
@@ -2,6 +2,8 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { serialize } from 'cookie';
+import { configDotenv } from 'dotenv';
+configDotenv()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end('Method Not Allowed');
